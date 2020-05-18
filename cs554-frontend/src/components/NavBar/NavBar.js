ab347947bba711a0cf8071ca75496eefdf63bf6d
vpilly <varun.pilly@gmail.com>
2020-05-17 22:47:27 -0400

    removed old firebase credits

--- cs554-frontend/src/components/NavBar/NavBar.js
+++ cs554-frontend/src/components/NavBar/NavBar.js
@@ -16,24 +16,3 @@ const NavBar = (props) => {
   );
 };
 export default NavBar;
-
-// Firebase App (the core Firebase SDK) is always required and
-// must be listed before other Firebase SDKs
-/*var firebase = require("firebase/app");
-
-// Add the Firebase products that you want to use
-require("firebase/auth");
-const firebaseConfig = {
-    apiKey: "AIzaSyDkKeivih0mPtcebcfh6-77UcJjI_Vqw70",
-    authDomain: "restaurant-57d38.firebaseapp.com",
-    databaseURL: "https://restaurant-57d38.firebaseio.com",
-    projectId: "restaurant-57d38",
-    storageBucket: "restaurant-57d38.appspot.com",
-    messagingSenderId: "234278647194",
-    appId: "1:234278647194:web:ec5a75e18ca012a2473623",
-    measurementId: "G-9WKNKXFQQR"
-  };
-  
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  */
